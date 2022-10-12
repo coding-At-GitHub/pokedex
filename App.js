dc42d56f8506db3015664a68c1f5843ba5fdd1f8
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2022-10-13 04:26:06 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokemon from './Component/Pokemon';
+import './Component/Style.css'
+function App() {
+  return (
+    <>
+      <Pokemon />
+      hi
+    </>
+  );
+}
+
+export default App;
