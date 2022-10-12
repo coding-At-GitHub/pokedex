554d4a11dcac1fb90c61b341210adb607731b606
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2022-10-13 01:05:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pokemon from './Component/Pokemon';
+// import './Component/Style.css'
+function App() {
+  return (
+    <>
+      <Pokemon />
+    </>
+  );
+}
+
+export default App;
